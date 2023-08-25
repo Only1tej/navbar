f8d8c2035fb2043833656170071d3eceb8e4da2d
only1tej <tijanienny3@gmail.com>
2023-08-24 23:32:38 -0700

    Final Update

--- src/App.js
+++ src/App.js
@@ -2,26 +2,35 @@ import Navbar from "./Navbar";
 import Pricing from "./pages/Pricing";
 import About from "./pages/About";
 import Home from "./pages/Home";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
-  let component;
-  switch (window.location.pathname) {
-    case "/":
-      component = <Home />;
-      break;
-    case "/pricing":
-      component = <Pricing />;
-      break;
-    case "/about":
-      component = <About />;
-      break;
-  }
-  console.log(window.location);
+  // Without the use of react router dom
+  // let component;
+  // switch (window.location.pathname) {
+  //   case "/":
+  //     component = <Home />;
+  //     break;
+  //   case "/pricing":
+  //     component = <Pricing />;
+  //     break;
+  //   case "/about":
+  //     component = <About />;
+  //     break;
+  // }
+  // console.log(window.location);
 
   return (
     <>
       <Navbar />
-      {component}
+      {/* <div className="container">{component}</div> */}
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/pricing" element={<Pricing />} />
+        </Routes>
+      </div>
     </>
   );
 }
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,17 +1,38 @@
+import { Link, useMatch, useResolvedPath } from "react-router-dom";
+
 export default function Navbar() {
   return (
     <nav className="nav">
-      <a href="/" className="site-title">
+      {/* Without the use of a react router dom */}
+      {/* <a href="/" className="site-title">
         Citizens
       </a>
       <ul>
-        <li className="active">
-          <a href="/pricing">Pricing</a>
-        </li>
-        <li>
-          <a href="/about">About</a>
-        </li>
+        <CustomLink href="/pricing">Pricing</CustomLink>
+        <CustomLink href="/about">About</CustomLink>
+      </ul> */}
+      <Link to="/" className="site-title">
+        Citizens
+      </Link>
+      <ul>
+        <CustomLink to="/pricing">Pricing</CustomLink>
+        <CustomLink to="/about">About</CustomLink>
       </ul>
     </nav>
+  );
+}
+//Without the react router dom, the 'href' is used instead of 'to'
+function CustomLink({ to, children, ...props }) {
+  // const path = window.location.pathname;
+
+  const resolvedPath = useResolvedPath(to);
+  const isActive = useMatch({ path: resolvedPath.pathname, end: true });
+  return (
+    // <li className={path === to ? "active" : ""}>
+    <li className={isActive ? "active" : ""}>
+      <Link to={to} {...props}>
+        {children}
+      </Link>
+    </li>
   );
 }
