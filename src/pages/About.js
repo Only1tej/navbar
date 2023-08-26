fb78bb8fe53bf6afca927c542c461544e0979fe4
only1tej <tijanienny3@gmail.com>
2023-08-25 23:54:37 -0700

    Update

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,3 +1,14 @@
 export default function About() {
-  return <h1>About</h1>;
+  return (
+    <>
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facilis ipsam
+      reprehenderit, placeat illo aspernatur tenetur quibusdam mollitia optio
+      praesentium magnam officiis possimus sed quis, dolorum eaque eveniet
+      ratione expedita aliquam? Vitae neque magni dolorum ab tenetur? Molestias
+      blanditiis vero iste soluta reprehenderit dolorum aut quia corporis
+      repudiandae saepe pariatur quos voluptatem recusandae nam eius quas eaque,
+      amet atque fuga? Soluta.
+      <h1>The best club in Manchester, England, and Europe.</h1>
+    </>
+  );
 }
